4c1b72152bd276b2f053acbba9569f8823df0461
Anran31 <55323042+Anran31@users.noreply.github.com>
2021-03-26 18:21:49 +0700

    edit soal1.sh

--- soal1/soal1.sh
+++ soal1/soal1.sh
@@ -1,30 +1,29 @@
 #!/bin/bash
 
-#soal 1 poin d
-#printf "Error,Count\n" > error_message.csv
-#grep "ERROR" syslog.log |cut -d " " -f7- | sort -n | sed "s/([^)]*)//g" | uniq -c | sort -nr | while read -r line ; do
-#ind=$(echo -n $line |cut -d " " -f2-);
-#val=$(echo -n $line|cut -d " " -f1);
+#1a Untuk mendapatkan jenis log, pesan log, dan username 
+#grep -oP "((?<=ticky\:\ ).*)" syslog.log
 
-#printf "${ind/$'\r'/'\b,'}${val}\n" >> error_message.csv
-#done 
+#1b dan 1d 
+#Untuk menampilkan semua pesan error yang muncul besserta jumlah kemunculannya
+#grep -oP "((?<=ERROR\ ).*?(?=\ \())" syslog.log | sort | uniq -c
 
-printf "ERROR,COUNT\n" > "error_message.csv" 
-grep -oP '(?<=ERROR\ ).*?(?=\ \()' syslog.log | sort | uniq -c | sort -nr | grep -oP '^ *[0-9]+ \K.*'| while read -r line; do
-
-msg=$(echo "$line")
-count=$(grep "$line" syslog.log | wc -l)
-
-printf "%s,%d\n" "$msg" "$count" >> "error_message.csv"
+#Untuk disimpan sesuai format
+printf "Error,Count\n" > error_message.csv
+grep -oP '(?<=ERROR\ ).*?(?=\ \()' syslog.log | sort | uniq -c | sort -nr | while read -r line; do
+count=$(grep -oP "([0-9].*?(?=\ ))" <<< "$line")
+message=$(grep -oP "(?<=\d\ ).*\w" <<< "$line")
+printf "%s,%d\n" "$message" "$count" >> error_message.csv
 done 
 
-#soal 1 poin e
-nameList=($(grep 'INFO\|ERROR' syslog.log | grep -oP '(?<=\().*?(?=\))' | sort | uniq))
-printf "Username,INFO,ERROR\n" > "user_statistic.csv"
-for i in ${nameList[@]};
-do
-  user=$i;
-  error=$(grep -wE "ERROR" syslog.log | grep -wE "$i" | wc -l);
-  info=$(grep -wE "INFO" syslog.log | grep -wE "$i" | wc -l);
-  printf "%s,%d,%d\n" "$user" "$info" "$error" >> "user_statistic.csv"
+
+#1c dan 1e
+#Untuk menampilkan seluruh user yang ada
+#grep -P "((?<=\().*?(?=\)))" syslog.log | sort | uniq
+
+#untuk menampilkan error dan info untuk setiap user yang ada
+printf "Username,INFO,ERROR\n" > user_statistic.csv
+grep -oP "((?<=\().*?(?=\)))" syslog.log | sort | uniq | while read -r user; do
+errorCount=$(grep -w "$user" syslog.log | grep "ERROR" | wc -l)
+infoCount=$(grep -w "$user" syslog.log | grep "INFO" | wc -l)
+printf "%s,%d,%d\n" "$user" "$infoCount" "$errorCount" >> user_statistic.csv
 done

417edfb55f28e1d5c3e7d1e91cc0ce61e5f8d66b
Anran31 <55323042+Anran31@users.noreply.github.com>
2021-03-27 14:43:43 +0700

    Revisi soal3b.sh

--- soal3/soal3b.sh
+++ soal3/soal3b.sh
@@ -2,8 +2,11 @@
 
 cd /home/anran/sisop/shift1/soal3
 dirName=$(date +"%d-%m-%Y")
-mkdir $dirName
 
-bash /home/anran/sisop/shift1/soal3/soal3a.sh
-mv Koleksi_{01..23} $dirName &> /dev/null
-mv Foto.log $dirName
+if [ -d "$dirName" ]; then
+    mkdir $dirName
+
+    bash /home/anran/sisop/shift1/soal3/soal3a.sh
+    mv Koleksi_{01..23} $dirName &> /dev/null
+    mv Foto.log $dirName
+fi

e158b49fc609b3c2624ff4c87903234cadda5ba8
Anran31 <55323042+Anran31@users.noreply.github.com>
2021-03-27 14:50:40 +0700

    Update soal3b.sh

--- soal3/soal3b.sh
+++ soal3/soal3b.sh
@@ -3,7 +3,7 @@
 cd /home/anran/sisop/shift1/soal3
 dirName=$(date +"%d-%m-%Y")
 
-if [ -d "$dirName" ]; then
+if [ ! -d "$dirName" ]; then
     mkdir $dirName
 
     bash /home/anran/sisop/shift1/soal3/soal3a.sh
